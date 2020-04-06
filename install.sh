f7789fe98beba964c1335b82dc355c681fc5e344
John Griffith <jmguru@gmail.com>
2020-04-06 17:32:27 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+RELEASE=humio
+NS=humio
+
 helm repo add humio https://humio.github.io/humio-helm-charts 
 helm repo update
-oc create namespace humio
-oc project humio
-helm install -f humio-values.yml -n humio --namespace humio humio/humio-helm-charts --tls
+oc create namespace $NS
+oc project $NS
+helm install -f humio-values.yml -n $RELEASE --namespace $NS humio/humio-helm-charts --tls
 oc create -f hostpath-scc.yml
 oc get scc humio-fluentbit
 oc adm policy add-scc-to-user humio-fluentbit system:serviceaccount:humio:humio-fluentbit-read
-#kubectl patch ds humio-fluentbit --patch "$(cat ./scc_patch.yml)"
-#oc get po | grep humio-core | awk '{print $1}' | xargs kubectl delete pods
-#oc get po | grep humio-fluentbit | awk '{print $1}' | xargs kubectl delete pods
 oc expose svc humio-humio-core-http
