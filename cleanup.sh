45f85f0cdcb617f22346d8ae6b0afdc99832db69
John Griffith <jmguru@gmail.com>
2020-04-06 17:33:34 -0500

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#helm3 uninstall humio
-helm delete humio --purge --tls
-kubectl delete namespace humio --cascade=true
+RELEASE=humio
+NS=humio
+
+helm delete $RELEASE --purge --tls
+kubectl delete namespace $NS --cascade=true
 oc delete scc humio-fluentbit
 oc delete ds humio-fluentbit
 oc adm policy remove-scc-from-user humio-fluentbit system:serviceaccount:humio:humio-fluentbit-read
