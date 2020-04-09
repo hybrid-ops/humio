da0e9ead5355d45ad4fda6909edc6f2776d9f09a
root <root@clobber1.fyre.ibm.com>
2020-04-08 21:51:19 -0700

    clean it up

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+NAMESPACE=humio
+RELEASENAME=humio
 
-#helm3 uninstall humio
-helm delete humio --purge --tls
-kubectl delete namespace humio --cascade=true
+helm delete $RELEASENAME --purge --tls
+kubectl delete namespace $NAMESPACE --cascade=true
 oc delete scc humio-fluentbit
 oc delete ds humio-fluentbit
 oc adm policy remove-scc-from-user humio-fluentbit system:serviceaccount:humio:humio-fluentbit-read
--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-helm repo add humio https://humio.github.io/humio-helm-charts 
+RELEASENAME=humio
+NAMESPACE=humio
+
+helm repo add $RELEASENAME https://humio.github.io/humio-helm-charts
 helm repo update
-oc create namespace humio
-oc project humio
-helm install -f humio-values.yml -n humio --namespace humio humio/humio-helm-charts --tls
+oc create namespace $NAMESPACE
+oc project $NAMESPACE
+helm install -f humio-values.yml -n $RELEASENAME --namespace $NAMESPACE humio/humio-helm-charts --tls
 oc create -f hostpath-scc.yml
 oc get scc humio-fluentbit
 oc adm policy add-scc-to-user humio-fluentbit system:serviceaccount:humio:humio-fluentbit-read
-#kubectl patch ds humio-fluentbit --patch "$(cat ./scc_patch.yml)"
-#oc get po | grep humio-core | awk '{print $1}' | xargs kubectl delete pods
-#oc get po | grep humio-fluentbit | awk '{print $1}' | xargs kubectl delete pods
+kubectl patch ds humio-fluentbit --patch "$(cat ./scc_patch.yml)"
+sleep 5
 oc expose svc humio-humio-core-http
+oc get po | grep humio-core | awk '{print $1}' | xargs kubectl delete pods
+oc get po | grep humio-fluentbit | awk '{print $1}' | xargs kubectl delete pods
